09f50f0e4594e3df753dd4856d3bd470ef11bd43
monsterkodi <monsterkodi@gmx.net>
2026-02-20 16:02:16 +0100

    outspace

--- world/BeltState.gd
+++ world/BeltState.gd
@@ -5,14 +5,19 @@ extends Node
 
 var type : int
 var pos : Vector2i
+var outIndex = 0
+var outRing  = [] 
 
 func _ready():
-    pass
+    
+    for dir in Belt.DIRS:
+        if type & Belt.OUTPUT[dir]:
+            outRing.push_back(dir)
+    Log.log("bs", type, outRing)
     
 func _exit_tree():
     
-    #assert(get_child_count() == 0)
-    Log.log("exit", name)
+    #Log.log("exit", name)
     Utils.fabState().beltStates.erase(pos)
     
 func advanceForDelta(delta:float) -> float:
@@ -30,31 +35,53 @@ func advanceItems(delta:float):
     
     assert(get_child_count() > 0)
     
-    var tailSpace = 1
     var advance = advanceForDelta(delta)
     
-    var headItem = get_child(0)
-    if headItem.advance + advance >= tailSpace:
-        var bs = Utils.fabState().beltStateAtPos(pos + Belt.NEIGHBOR[headItem.direction])
-        if bs:
-            var inDir = Belt.OPPOSITE[headItem.direction]
-            var adv = bs.hasSpace(inDir)
-            if adv >= 0:
-                remove_child(headItem)
-                headItem.advance = adv
-                bs.addItem(inDir, headItem)
+    for item in get_children():
+        if item.advance + advance >= 1:
+            var bs = Utils.fabState().beltStateAtPos(pos + Belt.NEIGHBOR[item.direction])
+            if bs:
+                var inDir = Belt.OPPOSITE[item.direction]
+                var adv = bs.hasSpace(inDir)
+                if adv >= 0:
+                    remove_child(item)
+                    item.advance = adv
+                    bs.addItem(inDir, item)
     
     for item in get_children():
-        if item != headItem:
-            tailSpace -= Belt.HALFSIZE
-        item.advance = minf(item.advance + advance, tailSpace)
-        tailSpace = item.advance - Belt.HALFSIZE
-        if item.advance > 0.5:
-            item.direction = Belt.outputDirForType(type)
+        if item.advance <= 0.5 and item.advance + advance > 0.5:
+            var space
+            for index in range(outRing.size()):
+                var ringIndex = (outIndex + index) % outRing.size()
+                var dir = outRing[ringIndex]
+                space = outSpace(dir, item.advance + advance)
+                if space > 0.5:
+                    item.advance = space
+                    item.direction = dir
+                    outIndex = (outIndex + 1) % outRing.size()
+                    break
+            if space <= 0.5:
+                item.advance = 0.5
+        else:
+            item.advance = minf(item.advance + advance, 1.0)
             
     if get_child_count() == 0:
-        Log.log("queue free", name, pos)
+        #Log.log("queue free", name, pos)
         queue_free()
+        
+func outSpace(dir, advance: float = 0.5) -> float:
+
+    if not type & Belt.OUTPUT[dir]:
+        return -2
+    
+    for item in get_children():
+        if item.direction == dir:
+            var space = item.advance - Belt.HALFSIZE
+            if space - Belt.HALFSIZE > 0.5:
+                return minf(advance, space - Belt.HALFSIZE)
+            else:
+                return space - Belt.HALFSIZE
+    return advance
     
 func hasSpace(dir, advance: float = 0) -> float:
 
@@ -63,7 +90,7 @@ func hasSpace(dir, advance: float = 0) -> float:
     
     if get_child_count() == 0:
         return advance
-    if get_child_count() > 2:
+    if get_child_count() > 3:
         return -1
     var tailSpace = get_child(-1).advance - Belt.HALFSIZE
     if tailSpace > Belt.HALFSIZE:
